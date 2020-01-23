62f75e648697e0549b8f7ce8a56fc38e9c8f2e44
Andres F. Moreno Sarria <idiotpeople95@gmail.com>
2020-01-22 21:33:11 -0500

    .

--- js/bar.js
+++ js/bar.js
@@ -5,3 +5,8 @@ function ver() {
 function ocultar() {
     document.getElementById('prod').style.display="none";
 }
+
+document.getElementById("b_menu").addEventListener("click",function(){
+  
+  document.getElementById("menu_oculto").classList.toggle("mostrar");
+});

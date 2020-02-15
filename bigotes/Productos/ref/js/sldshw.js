0ed5bf31ecdc1a0e2bf12e18c9ffd6e35f4c2386
Andres F. Moreno Sarria <idiotpeople95@gmail.com>
2020-02-14 20:57:08 -0500

    .

---
+++ bigotes/Productos/ref/js/sldshw.js
@@ -0,0 +1,76 @@
+$('.chngP').on({
+    'click': function(){
+        var img_mini=$('.chngP').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngA').on({
+    'click': function(){
+        var img_mini=$('.chngA').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngB').on({
+    'click': function(){
+        var img_mini=$('.chngB').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngC').on({
+    'click': function(){
+        var img_mini=$('.chngC').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngD').on({
+    'click': function(){
+        var img_mini=$('.chngD').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngE').on({
+    'click': function(){
+        var img_mini=$('.chngE').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngF').on({
+    'click': function(){
+        var img_mini=$('.chngF').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngG').on({
+    'click': function(){
+        var img_mini=$('.chngG').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngH').on({
+    'click': function(){
+        var img_mini=$('.chngH').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngI').on({
+    'click': function(){
+        var img_mini=$('.chngI').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});
+
+$('.chngJ').on({
+    'click': function(){
+        var img_mini=$('.chngJ').attr("src");
+        $('.principal-img-fl').attr("src",img_mini);
+    }
+});

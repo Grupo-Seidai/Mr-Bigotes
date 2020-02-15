79080c216a6c5e8e56d6e800671f730c7af9e684
Andres F. Moreno Sarria <idiotpeople95@gmail.com>
2020-02-15 00:11:47 -0500

    .

--- bigotes/Productos/ref/js/add_cart.js
+++ bigotes/Productos/ref/js/add_cart.js
@@ -1,13 +1,7 @@
 var x;
 function Sumbit(){
-    document.cookie=document.getElementById("Pname").innerHTML +'='+document.getElementById("Pname").innerHTML+'+'+document.getElementById("Pprice").innerHTML+'+'+$('.chngP').attr("src")+';expires=Fri, 31 Dec 2023 23:59:59 GMT; path=/';
+    document.cookie=document.getElementById("Pname").innerHTML +'=U+'+document.getElementById("Pname").innerHTML+'+'+document.getElementById("Pprice").innerHTML+'+'+$('.chngP').attr("src")+';expires=Fri, 31 Dec 2023 23:59:59 GMT; path=/';
     x=document.cookie;
-    alert(x);
 }
 
-function ask(){
-    x=document.cookie;
-
-    alert(x);
-
-}
+x=document.cookie;
